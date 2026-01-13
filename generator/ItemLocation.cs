0d8d118957418641d75216a5f4cbdd1a33d0063b
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-01-13 14:14:35 -0500

    gen: add door and item structs

---
+++ generator/ItemLocation.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace blas1wikigen;
+
+public class ItemLocation
+{
+    public string Id { get; }
+
+    public string Name { get; }
+
+    public string Logic { get; }
+
+    public string Room { get; }
+
+    [JsonConstructor]
+    public ItemLocation(string id, string name, string logic, string room)
+    {
+        Id = id;
+        Name = name;
+        Logic = logic;
+        Room = room;
+    }
+}
