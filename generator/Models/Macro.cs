4a6d82dd781db12199623011a680e2ce58ba4c0f
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-10 18:11:31 -0500

    gen: add macro type

---
+++ generator/Models/Macro.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace blas1wikigen.Models;
+
+public class Macro
+{
+    public string Name { get; }
+
+    public string Hint { get; }
+
+    [JsonConstructor]
+    public Macro(string name, string hint)
+    {
+        Name = name;
+        Hint = hint;
+    }
+}
